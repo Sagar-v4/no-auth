9d6634a85fdda22b5adf7498b4dfd4c2f0300901
Sagar-v4 <sagarvariya4@gmail.com>
2025-03-27 13:34:26 +0530

    fix(api keys): update data table

--- apps/web/src/skeletons/forms/responsive-dialog.tsx
+++ apps/web/src/skeletons/forms/responsive-dialog.tsx
@@ -7,27 +7,20 @@ import { Skeleton } from "@workspace/ui/components/skeleton";
 
 export function ResponsiveDialogSkeleton({
   className,
-}: React.ComponentProps<"div">) {
+  fielCount = 2,
+}: React.ComponentProps<"div"> & { fielCount?: number }) {
   return (
     <>
       <div className={cn("grid gap-4", className)}>
-        <div className="grid gap-2">
-          <Skeleton className="h-5 w-[100px]" />
-          <Skeleton className="h-8 w-full" />
-        </div>
-        <div className="grid gap-2">
-          <Skeleton className="h-5 w-[100px]" />
-          <Skeleton className="h-8 w-full" />
-        </div>
-        <div className="grid gap-2">
-          <Skeleton className="h-5 w-[100px]" />
-          <Skeleton className="h-8 w-full" />
-        </div>
-        <div className="grid gap-2">
-          <Skeleton className="h-5 w-[100px]" />
-          <Skeleton className="h-8 w-full" />
-        </div>
-        <Skeleton className="h-8 w-full bg-green-500" />
+        {Array.from({ length: fielCount }).map((_, index) => {
+          return (
+            <div key={index} className="grid gap-2">
+              <Skeleton className="h-3 w-[100px]" />
+              <Skeleton className="h-9 w-full" />
+            </div>
+          );
+        })}
+        <Skeleton className="h-9 w-full bg-green-500" />
       </div>
     </>
   );
